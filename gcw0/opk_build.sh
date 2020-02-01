e9eb7607bb0eff93c0d5a84d34375389ade4156b
Panja0 <Panja0@users.noreply.github.com>
2020-02-01 08:23:04 +0100

    Update opk_build.sh

--- gcw0/opk_build.sh
+++ gcw0/opk_build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-rm -rf genplus_sgg.opk
-mksquashfs gen_gcw0 opk-data/* genplus_sgg.opk -all-root -noappend -no-exports -no-xattrs
+rm -rf GenesisPlusGX_sgg.opk
+mksquashfs gen_gcw0 opk-data/* GenesisPlusGX_sgg.opk -all-root -noappend -no-exports -no-xattrs
